7b0d065e839ee76cc354cc66010b0aa2ce74c1ec
tanmay-1354 <78288407+tanmay-1354@users.noreply.github.com>
2025-12-30 16:45:32 +0530

    Update app.js

--- microservice/app.js
+++ microservice/app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from Microservice 🚀",
+    message: "Hello from Microservice Tanmay",
     status: "OK"
   });
 });
