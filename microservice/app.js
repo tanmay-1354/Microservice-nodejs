4ad945ae309bada2bdab2f859298a0295774c8b4
tanmay-1354 <78288407+tanmay-1354@users.noreply.github.com>
2025-12-30 15:59:14 +0530

    Create app.js

---
+++ microservice/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from Microservice 🚀",
+    status: "OK"
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("healthy");
+});
+
+app.listen(PORT, () => {
+  console.log(`Microservice running on port ${PORT}`);
+});
